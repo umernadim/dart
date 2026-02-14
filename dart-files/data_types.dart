3ede945b9fd314020baefbc6d7452c48311207f1
umernadim <itsumernadeem@gmail.com>
2026-02-14 22:10:53 +0500

    new files

---
+++ dart-files/data_types.dart
@@ -0,0 +1,21 @@
+void main() {
+  // numbers : int
+  int score = 23;
+  var count = 23;
+
+  // numbers: double
+  double percentage = 98.76;
+  var percent = 85.76;
+  double exponent = 1.62e4;
+
+  // strings
+  String name = "Umer";
+  var company = "Google";
+
+  // bolean
+  bool isValid = true;
+  var isAlive = false;
+
+  print(score);
+  print(name);
+  }
---
+++ dart-files/string_interpolation.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  // various ways to define string literals in dart
+  String s1 = 'Single';
+  String s2 = "Double";
+  String s3 = 'It\'s easy';
+  String s4 = "It\'s easy";
+
+  String s5 = 'This is going to be a very long string. ' 
+  'This is just a sample string demo in Dart programming.';
+
+  String name = "Umer";
+  print("My name is $name");
+  print("The number of characters in string is ${name.length}");
+
+  int l = 20;
+  int b = 12;
+
+  print("The sum of $l and $b is ${l+b}");
+
+
+}
