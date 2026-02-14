cf24e38f7d5de869f13a6b1516c51b0cfe38d666
umernadim <itsumernadeem@gmail.com>
2026-02-14 11:16:12 +0500

    first file

---
+++ dart-files/print.dart
@@ -0,0 +1,12 @@
+void main(){
+    // this is firt line of code
+    print("Hello World");
+
+    print('This is my first application');
+
+    // performing arithematic operation
+    print(4/2);
+
+    // printing bolean
+    print(false);
+}
